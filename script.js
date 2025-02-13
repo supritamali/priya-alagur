692acef79adb50f9c4debe3a991ab146626b37a3
supritamali <priyaalagur108@gmail.com>
2025-02-13 16:18:23 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,44 @@
-document.addEventListener("Counter",init);
+//Todo App
 
-let count = Number(sessionStorage.getItem("Count"))|| 0;
+document.addEventListener("Todos",init);
 
-function init() {
-    document.getElementById("count").textContent = count;
+function init(){
+    loadTasks();
 }
-function increaseCount() {
-    count++;
-    updateCount();
+
+function loadTasks(){
+    const tasks = JSON.parse(localStorage.getItem("tasks"))||[];
+tasks.forEach(task => addTaskToDOM(task));
 }
-function decreaseCount() {
-    count--;
-    updateCount();
+function addTask(){
+    const taskInput = document.getElementById("taskInput");
+    const task = taskInput.value.trim();
+    if(task == "") return;
+
+    addTaskToDOM(task);
+    saveTask(task);
+    taskInput.value="";
 }
-function resetCount() {
-    count = 0;
-    updateCount();
+function addTaskToDOM(task){
+    const taskList = document.getElementById("taskList");
+    const li = document.createElement("li");
+    li.innerHTML = `${task} <button class= "remove-btn">Remove</button>`
+    taskList.appendChild(li);
+    
+    li.querySelector(".remove-btn").addEventListener("click",function(){
+    removeTask(task,li);
+    })
 }
-function updateCount() {
-    document.getElementById("count").textContent = count;
-    sessionStorage.setItem("count", count);
+
+function saveTask(task){
+    const tasks = JSON.parse(localStorage.getItem("tasks"))||[];
+    tasks.push(task);
+    localStorage.setItem("tasks",JSON.stringify(tasks));
+}
+
+function removeTask(task,element){
+    let tasks = JSON.parse(localStorage.getItem("tasks"));
+    tasks = tasks.filter(t => t!==task);
+    localStorage.setItem("tasks",JSON.stringify(tasks));
+    element.remove();
 }
